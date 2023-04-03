d78d14f150cd6909f12bb9d17bacf3bbf3e66169
patrizineversuxxx <r2tr@yandex.ru>
2023-04-03 12:39:20 +0400

    Changed calculation target value

--- Random_Device_Selection.py
+++ Random_Device_Selection.py
@@ -4,65 +4,67 @@ from Data import *
 from Parser import *
 
 
-def check_department_target(department):
-    return int(0.12 * len(department.user_list))+1
 
 
-path = r'C:\KEK\ID_RANDOM.xlsx'
-workbook = openpyxl.load_workbook(path)
-spreadsheet = workbook.active
+def check_department_target(department, params):
+    target_percent = params[target_percent]
+    return int(target_percent * len(department.user_list))+1
 
-department_list = {}
-user_list = {}
-device_list = []
 
-target_percent = 0.12
-get_data_from_xlsx(spreadsheet, department_list, user_list, device_list)
+def random_selection(params:dict):
+    path = params['middle_file']
+    workbook = openpyxl.load_workbook(path)
+    spreadsheet = workbook.active
 
-departments = list(department_list.values())
-user_list = list(user_list.values())
+    department_list = {}
+    user_list = {}
+    device_list = []
 
-aad_map = {}
-# Selecting AAD_Joined devices
-for department in departments:
-    device_user = {}
-    for user in department.user_list:
-        for device in user.device_list:
-            if device.group == 'AAD_Joined':
-                device_user[user] = device
-                aad_map[department] = device_user
+    get_data_from_xlsx(spreadsheet, department_list, user_list, device_list)
 
-result_map = {}
+    departments = list(department_list.values())
+    user_list = list(user_list.values())
 
-# Selecting 45 AAD_Joined devices for Pilot group
-target = 45
-count = 0
+    aad_map = {}
+    # Selecting AAD_Joined devices
+    for department in departments:
+        device_user = {}
+        for user in department.user_list:
+            for device in user.device_list:
+                if device.group == 'AAD_Joined':
+                    device_user[user] = device
+                    aad_map[department] = device_user
 
-while count < target:
-    department = random.choice(departments)
-    if (department in aad_map): 
-        temp = list(aad_map[department].items())[0]
-        user_device = {}
-        user_device[temp[0]]= temp[1]
-        aad_map.pop(department)
-        result_map[department] = user_device
-        count += 1
-    departments.remove(department)
-    
-departments = list(department_list.values())
+    result_map = {}
 
-for department in departments:
-    target = int(len(department.user_list) * 0.12)+1
-    if department in result_map:
-        while len(result_map[department]) < target:
+    # Selecting 45 AAD_Joined devices for Pilot group
+    target = 45
+    count = 0
+
+    while count < target:
+        department = random.choice(departments)
+        if (department in aad_map):
+            temp = list(aad_map[department].items())[0]
+            user_device = {}
+            user_device[temp[0]] = temp[1]
+            aad_map.pop(department)
+            result_map[department] = user_device
+            count += 1
+        departments.remove(department)
+
+    departments = list(department_list.values())
+
+    for department in departments:
+        target = check_department_target(department, params)
+        if department in result_map:
+            while len(result_map[department]) < target:
+                user = random.choice(department.user_list)
+                device = random.choice(user.device_list)
+                result_map[department][user] = device
+        else:
             user = random.choice(department.user_list)
             device = random.choice(user.device_list)
-            result_map[department][user]= device
-    else:
-        user = random.choice(department.user_list)
-        device = random.choice(user.device_list)
-        result_map[department] = {}
-        result_map[department][user] = device
+            result_map[department] = {}
+            result_map[department][user] = device
 
-
-save_data_to_xlsx(result_map)
+    save_data_to_xlsx(result_map)
