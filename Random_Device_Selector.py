97388eee132aa9e6bbd079ef0a9dbbf4a25321eb
patrizineversuxxx <r2tr@yandex.ru>
2023-04-21 12:13:48 +0400

    Created atype annotations

--- Random_Device_Selector.py
+++ Random_Device_Selector.py
@@ -5,11 +5,14 @@ from Parser import *
 from DataTable import *
 
 
-def check_department_target(department, params):
+def check_department_target(department: Department, params: typing.Dict):
     target_percent = params['target_percent']
     return int(target_percent * len(department.user_list))+1
 
-def minimal_group(params, result_map, department_map) :
+
+def minimal_group(params: typing.Dict,
+        result_map: typing.Dict,
+        department_map: typing.Dict) -> typing.Dict(Department, typing.Dict(User, Device)):
     minimal_group_map = {}
     # Selecting minimal group devices
     for department in departments:
@@ -19,7 +22,7 @@ def minimal_group(params, result_map, department_map) :
                 if device.group == params["minimal_selected_group"]:
                     device_user[user] = device
                     minimal_group_map[department] = device_user
-    
+
     # Selecting minimal target devices for Pilot group
     minimal_target = params['minimal_target']
     count = 0
@@ -37,14 +40,16 @@ def minimal_group(params, result_map, department_map) :
 
     departments = list(department_map.values())
 
-def check_device_count(group, needed, requerments):
+
+def check_device_count(group: str, needed: typing.Dict, requerments: typing.Dict) -> bool:
     if needed[group] >= requerments[group]:
         return True
-    else: 
-        needed[group] +=1
+    else:
+        needed[group] += 1
         return False
 
-def random_selection(params: dict):
+
+def random_selection(params: typing.Dict):
     path = params['middle_file']
     workbook = openpyxl.load_workbook(path)
     spreadsheet = workbook.active
@@ -57,10 +62,10 @@ def random_selection(params: dict):
 
     departments = list(department_map.values())
     user_map = list(user_map.values())
-    result_map = {}  
-    requerments = params['required']
-    needed = {"AAD_Joined":0, "Hybrid_Joined":0, "macOS":0}
+    result_map = {}
 
+    requerments = params['required']
+    needed = {"AAD_Joined": 0, "Hybrid_Joined": 0, "macOS": 0}
 
     for department in departments:
         department_target = check_department_target(department, params)
@@ -76,8 +81,7 @@ def random_selection(params: dict):
             device = random.choice(user.device_list)
             if check_device_count(device.group, needed, requerments):
                 continue
-            result_map[department] = {}            
+            result_map[department] = {}
             result_map[department][user] = device
-            
 
     save_data_to_xlsx(result_map, params)
