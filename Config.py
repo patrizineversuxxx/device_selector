9da9b54bd5c150fcd955c8bc379b253eec1ca74c
patrizineversuxxx <r2tr@yandex.ru>
2023-04-21 15:31:23 +0400

    Changed Config class content

--- Config.py
+++ Config.py
@@ -2,52 +2,45 @@ import typing
 
 
 class Config:
-    def __init__(self, redirect_uri: str, client_credentials: str, client_id: str, authority_url: str, scopes: list[str], file_paths: typing.Dict[str, str]):
+    def __init__(
+            self, 
+            redirect_uri: str,
+            client_credentials: str,
+            client_id: str,
+            authority_url: str,
+            scopes: list[str],
+            file_paths: typing.Dict[str, str],
+            selection_conditions: typing.Dict
+        ):
+        
         self._redirect_uri = redirect_uri
         self._client_credentials = client_credentials
         self._client_id = client_id
         self._authority_url = authority_url
         self._scopes = scopes
         self._file_paths = file_paths
+        self._selection_conditions = selection_conditions
 
     @property
     def redirect_uri(self) -> str:
         return self._redirect_uri
-    
+
     @property
     def client_credentials(self) -> str:
         return self._client_credentials
-    
+
     @property
     def client_id(self) -> str:
         return self._client_id
-    
+
     @property
     def authority_url(self) -> str:
         return self._authority_url
-    
+
     @property
     def scopes(self) -> list:
         return self._scopes
-    
+
     @property
-    def file_paths(self) -> typing.Dict[str,str]:
+    def file_paths(self) -> typing.Dict[str, str]:
         return self._file_paths
-
-
-"""
-
-
-    "naming_tags": [
-        "EVN",
-        "MOW"
-    ],
-    "office_locations": [
-        "AM+Yerevan",
-        "RU+Moscow",
-        "RU+Novosibirsk"
-    ],
-    "target_percent": 0.12,
-    "minimal_target": 45,
-    "minimal_selected_group" : "AAD_Joined",
-    "required" : {"AAD_Joined":15, "Hybrid_Joined":30, "macOS":15}"""
--- Main.py
+++ Main.py
@@ -6,37 +6,47 @@ from Entities import *
 from Random_Device_Selector import *
 from MS_Graph_Connector import *
 
-# Creating an entity for using configurational parameters
-params = open_json("config.json")
+#Reading all of the configurational files
+connection_parameters = open_json("config.json")
 file_paths = open_json("file_paths.json")
-config = Config(redirect_uri=params['client_id'], client_credentials=params['client_id'], authority_url=params['authority_url'], scopes=params['scopes'], file_paths=file_paths)
+selection_conditions = open_json("selection_conditions.json")
+
+# Creating an entity for using configurational parameters
+config = Config(
+        redirect_uri=connection_parameters['client_id'], 
+        client_credentials=connection_parameters['client_id'], 
+        authority_url=connection_parameters['authority_url'], 
+        scopes=connection_parameters['scopes'], 
+        file_paths=file_paths, 
+        selection_conditions = selection_conditions
+    )
 
 # Creating a connection to API and saving requests headers
-headers = connect_to_api(params=params) #   params['redirect_uri'] params['client_credentials']
+headers = connect_to_api(params=connection_parameters) #   params['redirect_uri'] params['client_credentials']
 
 # Grabbing all of the users info from MS Graph
-users = user_to_json_grabbing(headers=headers, params=params)
+users = user_to_json_grabbing(headers=headers, params=connection_parameters)
 
 # Saving users info into the JSON file
-save_json(data=users, file_path=params['path_user'])
+save_json(data=users, file_path=connection_parameters['path_user'])
 
 # Grabbing all of the devices info from MS Graph
-devices = device_to_json_grabbing(headers=headers, params=params)
+devices = device_to_json_grabbing(headers=headers, params=connection_parameters)
 
 # Saving devices info into the JSON file
-save_json(data=users, file_path=params['path_device'])
+save_json(data=users, file_path=connection_parameters['path_device'])
 
 # Creating departments from users and devices data
-departmens = get_data_from_json(params=params)
+departmens = get_data_from_json(params=connection_parameters)
 
 # Saving records in xlsx table
-save_data_to_xlsx_prepational_step(departmens, params)
+save_data_to_xlsx_prepational_step(departmens, connection_parameters)
 
 # Deleting records from previous table, which contains filtered job titles, and saves the result in the another xlsx file
-check_xlsx_for_vip(params=params)
+check_xlsx_for_vip(params=connection_parameters)
 
 # Randomly selecting needed devices using user's conditions
-result = random_selection(params=params)
+result = random_selection(params=connection_parameters)
 
 # Saving the result in the xlsx table
-save_data_to_xlsx(result, params)
+save_data_to_xlsx(result, connection_parameters)
