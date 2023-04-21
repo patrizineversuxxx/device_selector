cdfbb7f85df1ca5f5e36a3f8b0174367781b17d4
patrizineversuxxx <r2tr@yandex.ru>
2023-04-21 15:23:22 +0400

    Created an instanse of Config class in Main module

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from Config import *
 from JSON_Parser import *
 from MS_Graph_Data_Grabber import *
 from Job_Title_Selector import *
@@ -7,9 +8,11 @@ from MS_Graph_Connector import *
 
 # Creating an entity for using configurational parameters
 params = open_json("config.json")
+file_paths = open_json("file_paths.json")
+config = Config(redirect_uri=params['client_id'], client_credentials=params['client_id'], authority_url=params['authority_url'], scopes=params['scopes'], file_paths=file_paths)
 
 # Creating a connection to API and saving requests headers
-headers = connect_to_api(params=params)
+headers = connect_to_api(params=params) #   params['redirect_uri'] params['client_credentials']
 
 # Grabbing all of the users info from MS Graph
 users = user_to_json_grabbing(headers=headers, params=params)
