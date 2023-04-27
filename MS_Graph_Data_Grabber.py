9cdb14b3c2b5efa5d64c5aef7e8f942cd9bd0b41
patrizineversuxxx <r2tr@yandex.ru>
2023-04-27 17:13:39 +0400

    Created a progress check for usability

--- MS_Graph_Data_Grabber.py
+++ MS_Graph_Data_Grabber.py
@@ -9,7 +9,7 @@ def get_users_from_API(headers, office_locations):
     all_users = []
 
     for office_location in office_locations:
-        next_link = r"https://graph.microsoft.com/v1.0/users?$count=true&$filter=startswith(officeLocation,'" + office_location + "+')"+ \
+        next_link = r"https://graph.microsoft.com/v1.0/users?$count=true&$filter=startswith(officeLocation,'" + office_location + "+')" + \
             r"+and+onPremisesExtensionAttributes/extensionAttribute11+eq+'Employee'+and+accountEnabled+eq+true&$select=id,displayName,mail,jobTitle,officeLocation,department"
         while next_link:
             response = requests.get(next_link, headers=headers)
@@ -17,6 +17,10 @@ def get_users_from_API(headers, office_locations):
             all_users += json_data['value']
             next_link = json_data.get("@odata.nextLink")
 
+    counter = 0
+    user_count = len(all_users)
+    persent = user_count/100
+    prev_progress = 0
     for user in all_users:
         if user.get('id'):
             manager_url = r'https://graph.microsoft.com/v1.0/users/' + \
@@ -26,18 +30,24 @@ def get_users_from_API(headers, office_locations):
             user['manager_name'] = manager_data.get('displayName')
             user['manager_mail'] = manager_data.get('mail')
             devices_url = r'https://graph.microsoft.com/v1.0/users/' + \
-            user["id"]+r'/ownedDevices?$count=true&$filter=isManaged+ne+false&$select=displayName,id,enrollmentType,operatingSystem,' + \
-            r'isManaged,approximateLastSignInDateTime'
+                user["id"]+r'/ownedDevices?$count=true&$filter=isManaged+ne+false&$select=displayName,id,enrollmentType,operatingSystem,' + \
+                r'isManaged,approximateLastSignInDateTime'
             devices_response = requests.get(devices_url, headers=headers)
             devices_data = devices_response.json()
             user['devices'] = devices_data.get('value')
+            counter += 1
+            progress = int(counter/persent)
+            if (progress > prev_progress):
+                prev_progress = progress
+                print(prev_progress, r"% completed")
 
+    print("Data grabbing completed!")
     return all_users
 
 
 def get_intune_devices_from_API(headers, naming_tags):
     all_devices = []
-    
+
     for naming_tag in naming_tags:
         next_link = r"https://graph.microsoft.com/beta/deviceManagement/managedDevices?$filter=startswith(devicename,'"+naming_tag + \
             r"')&select=devicename,userid,azureActiveDirectoryDeviceId,deviceEnrollmentType,operatingSystem,usersLoggedOn"
