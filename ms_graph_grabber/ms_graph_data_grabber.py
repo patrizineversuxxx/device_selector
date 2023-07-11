3d27d4e09db6ed5de9cf5cc67ff018c413ede122
patrizineversuxxx <r2tr@yandex.ru>
2023-07-11 18:33:39 +0400

    Changed formatting

--- ms_graph_grabber/ms_graph_data_grabber.py
+++ ms_graph_grabber/ms_graph_data_grabber.py
@@ -5,12 +5,14 @@ from file_recorder.json_parser import *
 
 
 def invoke_cost_center(user: typing.Dict):
-    user['cost_center'] = user.get("onPremisesExtensionAttributes").get("extensionAttribute15")
+    user['cost_center'] = user.get(
+        "onPremisesExtensionAttributes").get("extensionAttribute15")
     del user['onPremisesExtensionAttributes']
+
 
 def fetch_manager_info(headers: typing.Dict, user: typing.Dict):
     manager_url = r'https://graph.microsoft.com/beta/users/' + \
-                user['id']+r'/manager?$select=displayName,mail'
+        user['id']+r'/manager?$select=displayName,mail'
 
     manager_response = requests.get(manager_url, headers=headers)
     manager_data = manager_response.json()
@@ -18,26 +20,29 @@ def fetch_manager_info(headers: typing.Dict, user: typing.Dict):
     user['manager_name'] = manager_data.get("displayName")
     user['manager_mail'] = manager_data.get("mail")
 
+
 def fetch_devices_info(headers: typing.Dict, user: typing.Dict):
     devices_url = r'https://graph.microsoft.com/beta/users/' + \
-                user['id']+r'/ownedDevices?$select=displayName,id,enrollmentType,operatingSystem,' + \
-                r'isManaged,approximateLastSignInDateTime,manufacturer,model'
+        user['id']+r'/ownedDevices?$select=displayName,id,enrollmentType,operatingSystem,' + \
+        r'isManaged,approximateLastSignInDateTime,manufacturer,model'
 
     devices_response = requests.get(devices_url, headers=headers)
     devices_data = devices_response.json()
 
     user['devices'] = devices_data.get("value")
 
+
 def process_user_data(headers: typing.Dict, user: typing.Dict):
     fetch_manager_info(headers, user)
     fetch_devices_info(headers, user)
-    
+
+
 def get_users_from_API(headers: typing.Dict) -> typing.Dict:
     start = time.time()
     all_users = []
-    
+
     next_link = r"https://graph.microsoft.com/beta/users?$count=true&$filter=onPremisesExtensionAttributes/extensionAttribute11+eq+'Employee'+and+accountEnabled+eq+true&$select=id,displayName,mail,jobTitle,officeLocation,department,onPremisesExtensionAttributes"
-    
+
     while next_link:
         response = requests.get(next_link, headers=headers)
         json_data = response.json()
@@ -47,17 +52,16 @@ def get_users_from_API(headers: typing.Dict) -> typing.Dict:
     user_count = len(all_users)
 
     print("Today we have ", user_count, "users")
-    
+
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
         for user in all_users:
-            if user.get('id'):            
+            if user.get('id'):
                 invoke_cost_center(user)
-                futures.append(executor.submit(process_user_data, headers, user))
+                futures.append(executor.submit(
+                    process_user_data, headers, user))
         for future in concurrent.futures.as_completed(futures):
             pass
-
-
 
     end = time.time()
     print("Data grabbing completed!", user_count)
