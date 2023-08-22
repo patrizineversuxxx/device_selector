106fd0b994a27040a38df18fc680b5edd2fe58c0
patrizineversuxxx <r2tr@yandex.ru>
2023-08-22 16:01:09 +0400

    Removed unnessesary things from data grabbing runner script

--- shrek.py
+++ shrek.py
@@ -1,13 +1,5 @@
-from file_recorder.json_parser import open_json
-from model.department_converter import parse_affected
-from selector.main import device_selector_flow
 from ms_graph_grabber.data_grabber_flow import data_grabber_flow
-from selector.сonfig import get_config
 
 
-# Reading the configuration files
-#configuration = get_config()
-#data_grabber_flow()
-#affected_users = get_affected_users_and_devices()
-#print(3)
-#device_selector_flow(configuration)
+# Start Data Grabbing Process
+data_grabber_flow()
