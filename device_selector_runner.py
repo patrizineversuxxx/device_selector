6199462183680680844a2404fabf706c0c338986
patrizineversuxxx <r2tr@yandex.ru>
2023-08-23 15:09:05 +0400

    Changed selector runner behavior

---
+++ device_selector_runner.py
@@ -0,0 +1,5 @@
+from selector.main import device_selector_flow
+
+
+# Start Data Grabbing Process
+device_selector_flow()
--- shrek.py
+++
@@ -1,5 +0,0 @@
-from ms_graph_grabber.data_grabber_flow import data_grabber_flow
-
-
-# Start Data Grabbing Process
-data_grabber_flow()
