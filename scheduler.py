a85ccf39bb8a7e43d0db836c984cadc87b15a40b
patrizineversuxxx <r2tr@yandex.ru>
2023-08-23 13:49:53 +0400

    Updated scheduler with dostrings and separated time var

--- scheduler.py
+++ scheduler.py
@@ -3,24 +3,40 @@ import schedule
 import time
 from ms_graph_grabber.data_grabber_flow import data_grabber_flow
 
+# Define the time at which the script should be launched daily
+LAUNCH_TIME = "19:05"
+
+
 def configure_logging():
+    """
+    Configures the logging settings for the script.
+    """
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
+
 def schedule_data_grabbing():
-    # Schedule the script to be launched every day at a specific time (e.g., 19:05)
-    schedule.every().day.at("19:05").do(data_grabber_flow)
+    """
+    Schedules the data grabbing flow using the specified launch time.
+    """
+    # Schedule the script to be launched every day at a specific time
+    schedule.every().day.at(LAUNCH_TIME).do(data_grabber_flow)
+
 
 def main():
+    """
+    Main function that orchestrates the script's execution.
+    """
     configure_logging()
 
     try:
         schedule_data_grabbing()
 
         # Message for the user
-        print("User's and device data will be updated at 19:05 Pacific Time")
+        logging.info(
+            f"User's and device data will be updated at {LAUNCH_TIME} Pacific Time")
 
         while True:
             schedule.run_pending()
@@ -29,6 +45,7 @@ def main():
         logging.info("Script stopped by user.")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
+
 
 # Entry point of the script
 if __name__ == "__main__":
