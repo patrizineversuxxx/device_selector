8fb28789a077693f9924c9b594442cc84f87ddde
patrizineversuxxx <r2tr@yandex.ru>
2023-08-23 16:23:33 +0400

    Removed useless functionality

--- selector/random_device_selector.py
+++ selector/random_device_selector.py
@@ -9,36 +9,6 @@ def check_department_target(department: Department, params: typing.Dict) -> int:
     return int(target_percent * len(department.user_list))+1
 
 
-def get_minimal_group_devices(selection_conditions: typing.Dict, result_map: typing.Dict,
-                              department_map: typing.Dict) -> typing.Dict[Department, typing.Dict[User, Device]]:
-    minimal_group_map = {}
-    # Selecting minimal group devices
-    for department in departments:
-        device_user = {}
-        for user in department.user_list:
-            for device in user.device_list:
-                if device.group == selection_conditions["minimal_selected_group"]:
-                    device_user[user] = device
-                    minimal_group_map[department] = device_user
-
-    # Selecting minimal target devices for Pilot group
-    minimal_target = selection_conditions['minimal_target']
-    count = 0
-
-    while count < minimal_target:
-        department = random.choice(departments)
-        if (department in minimal_group_map):
-            temp = list(minimal_group_map[department].items())[0]
-            user_device = {}
-            user_device[temp[0]] = temp[1]
-            minimal_group_map.pop(department)
-            result_map[department] = user_device
-            count += 1
-        departments.remove(department)
-
-    departments = list(department_map.values())
-
-
 def check_device_count(group: str, needed: typing.Dict, requirements: typing.Dict) -> bool:
     if needed[group] >= requirements[group]:
         return True
