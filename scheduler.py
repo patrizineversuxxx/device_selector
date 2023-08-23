f91c75af43e82e8ff972a140cea2b31906220a28
patrizineversuxxx <r2tr@yandex.ru>
2023-08-23 13:47:23 +0400

    Updated scheduler module

--- scheduler.py
+++ scheduler.py
@@ -3,14 +3,21 @@ import schedule
 import time
 from ms_graph_grabber.data_grabber_flow import data_grabber_flow
 
-# Configure logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+def schedule_data_grabbing():
+    # Schedule the script to be launched every day at a specific time (e.g., 19:05)
+    schedule.every().day.at("19:05").do(data_grabber_flow)
 
 def main():
+    configure_logging()
+
     try:
-        # Schedule the script to be launched every day at a specific time (e.g., 19:05)
-        schedule.every().day.at("13:25").do(data_grabber_flow)
+        schedule_data_grabbing()
 
         # Message for the user
         print("User's and device data will be updated at 19:05 Pacific Time")
