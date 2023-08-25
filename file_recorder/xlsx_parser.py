efe0b7dd417f13f33192ff28bc38de9ea81485d3
patrizineversuxxx <r2tr@yandex.ru>
2023-08-25 14:57:01 +0400

    Updated xlsx parser and changed small mistakes in logs

--- file_recorder/xlsx_parser.py
+++ file_recorder/xlsx_parser.py
@@ -1,110 +1,166 @@
 import openpyxl
+from collections import namedtuple
 from model.entities import *
 
 
+# Named tuple for storing table's header
+Header = namedtuple(
+    'Header', ['Device_name', 'Device_id', 'Device_Group', 'Device_os', 'Device_enrollment_type',
+               'Device_type', 'Last_checkin_date', 'User_id', 'User_name', 'User_mail', 'Manager_name',
+               'Manager_mail', 'Job_title', 'Location', 'Cost_center', 'Department_name',
+               'Current_Device_Pilot_Group', 'Current_User_Pilot_Group']
+)
+# Named tuple for storing order for rows
+Row = namedtuple(
+    'Row', ['device', 'user', 'department_name']
+)
+
+# Constant defintions for table rows
+DEVICE_NAME_IDX = 0
+DEVICE_ID_IDX = 1
+DEVICE_GROUP_IDX = 2
+DEVICE_OS_IDX = 3
+DEVICE_ENROLLMENT_TYPE_IDX = 4
+DEVICE_TYPE_IDX = 5
+DEVICE_LAST_CHECKIN_DATE_IDX = 6
+USER_ID_IDX = 7
+USER_NAME_IDX = 8
+USER_MAIL_IDX = 9
+USER_MANAGER_NAME_IDX = 10
+USER_MANAGER_MAIL_IDX = 11
+USER_JOB_TITLE_IDX = 12
+USER_LOCATION_IDX = 13
+USER_COST_CENTER_IDX = 14
+DEPARTMENT_NAME_IDX = 15
+DEVICE_AFFECTED_IDX = 16
+USER_AFFECTED_IDX = 17
+
+HEADER = Header._fields
+
+
 def create_table_header(result_sheet):
-    result_sheet.cell(row=1, column=1, value='Device_name')
-    result_sheet.cell(row=1, column=2, value='Device_id')
-    result_sheet.cell(row=1, column=3, value='Device_Group')
-    result_sheet.cell(row=1, column=4, value='Device_os')
-    result_sheet.cell(row=1, column=5, value='Device_enrollment_type')
-    result_sheet.cell(row=1, column=6, value='Device_type')
-    result_sheet.cell(row=1, column=7, value='Last_checkin_date')
-    result_sheet.cell(row=1, column=8, value='User_id')
-    result_sheet.cell(row=1, column=9, value='User_name')
-    result_sheet.cell(row=1, column=10, value='User_mail')
-    result_sheet.cell(row=1, column=11, value='Manager_name')
-    result_sheet.cell(row=1, column=12, value='Manager_mail')
-    result_sheet.cell(row=1, column=13, value='Job_title')
-    result_sheet.cell(row=1, column=14, value='Location')
-    result_sheet.cell(row=1, column=15, value='Cost center')
-    result_sheet.cell(row=1, column=16, value='Department name')
-    result_sheet.cell(row=1, column=17, value='Current Device Pilot Group')
-    result_sheet.cell(row=1, column=18, value='Current User Pilot Group')
+    """Create the header row in the result spreadsheet.
+
+    Args:
+        result_sheet (openpyxl.worksheet.worksheet.Worksheet): The worksheet to add the header row to.
+    """
+    for idx, field in enumerate(HEADER, start=1):
+        result_sheet.cell(row=1, column=idx, value=field)
 
 
-def create_table_row(result_sheet, row_counter, device, user, department_name):
-    result_sheet.cell(row=row_counter, column=1, value=device.name)
-    result_sheet.cell(row=row_counter, column=2, value=device.id)
-    result_sheet.cell(row=row_counter, column=3, value=device.group)
-    result_sheet.cell(row=row_counter, column=4, value=device.os)
-    result_sheet.cell(row=row_counter, column=5, value=device.enrollment_type)
-    result_sheet.cell(row=row_counter, column=6, value=device.type)
-    result_sheet.cell(row=row_counter, column=7,
-                      value=device.last_checkin_date)
-    result_sheet.cell(row=row_counter, column=8, value=user.id)
-    result_sheet.cell(row=row_counter, column=9, value=user.name)
-    result_sheet.cell(row=row_counter, column=10, value=user.mail)
-    result_sheet.cell(row=row_counter, column=11,
-                      value=user.manager_name)
-    result_sheet.cell(row=row_counter, column=12,
-                      value=user.manager_mail)
-    result_sheet.cell(row=row_counter, column=13, value=user.job_title)
-    result_sheet.cell(row=row_counter, column=14, value=user.location)
-    result_sheet.cell(row=row_counter, column=15,
-                      value=user.cost_center)
-    result_sheet.cell(row=row_counter, column=16,
-                      value=department_name)
-    result_sheet.cell(row=row_counter, column=17,
-                      value=device.affected)
-    result_sheet.cell(row=row_counter, column=18,
-                      value=user.affected)
+def create_table_row(result_sheet, row_counter, row):
+    """Create a row of data in the result spreadsheet.
+
+    Args:
+        result_sheet (openpyxl.worksheet.worksheet.Worksheet): The worksheet to add the row to.
+        row_counter (int): The row number to add the data to.
+        row (Row): A Row namedtuple containing device, user, and department information.
+    """
+    device = row.device
+    user = row.user
+    department_name = row.department_name
+
+    row_data = [
+        device.name, device.id, device.group, device.os, device.enrollment_type, device.type,
+        device.last_checkin_date, user.id, user.name, user.mail, user.manager_name,
+        user.manager_mail, user.job_title, user.location, user.cost_center, department_name,
+        device.affected, user.affected
+    ]
+
+    for idx, value in enumerate(row_data, start=1):
+        result_sheet.cell(row=row_counter, column=idx, value=value)
+
+
+def extract_user_data(row):
+    """Extract user and device data from a spreadsheet row.
+
+    Args:
+        row (iterable): A row of data from the spreadsheet.
+
+    Returns:
+        Tuple[Device, User, str]: A tuple containing the extracted Device, User, and department name.
+    """
+    device_name = row[DEVICE_NAME_IDX]
+    device_id = row[DEVICE_ID_IDX]
+    device_group = row[DEVICE_GROUP_IDX]
+    device_os = row[DEVICE_OS_IDX]
+    device_enrollment_type = row[DEVICE_ENROLLMENT_TYPE_IDX]
+    device_type = row[DEVICE_TYPE_IDX]
+    device_last_checkin_date = row[DEVICE_LAST_CHECKIN_DATE_IDX]
+
+    user_id = row[USER_ID_IDX]
+    user_name = row[USER_NAME_IDX]
+    user_mail = row[USER_MAIL_IDX]
+    user_manager_name = row[USER_MANAGER_NAME_IDX]
+    user_manager_mail = row[USER_MANAGER_MAIL_IDX]
+    user_job_title = row[USER_JOB_TITLE_IDX]
+    user_location = row[USER_LOCATION_IDX]
+    user_cost_center = row[USER_COST_CENTER_IDX]
+    department_name = row[DEPARTMENT_NAME_IDX]
+    device_affected = row[DEVICE_AFFECTED_IDX]
+    user_affected = row[USER_AFFECTED_IDX]
+
+    device = Device(id=device_id, affected=device_affected, name=device_name, group=device_group,
+                    os=device_os, enrollment_type=device_enrollment_type, type=device_type,
+                    last_checkin_date=device_last_checkin_date)
+
+    user = User(id=user_id, affected=user_affected, name=user_name, mail=user_mail, manager_name=user_manager_name,
+                manager_mail=user_manager_mail, job_title=user_job_title, location=user_location,
+                cost_center=user_cost_center)
+
+    return device, user, department_name
+
+
+def process_spreadsheet(worksheet):
+    """Process rows in the spreadsheet and yield extracted user data.
+
+    Args:
+        worksheet (openpyxl.worksheet.worksheet.Worksheet): The worksheet to process.
+
+    Yields:
+        Tuple[Device, User, str]: A tuple containing the extracted Device, User, and department name.
+    """
+    for row in worksheet.iter_rows(min_row=2):
+        yield extract_user_data(row)
 
 
 def get_data_from_xlsx(path: str):
+    """Extract data from the input spreadsheet.
 
-    workbook = openpyxl.load_workbook(path)
-    spreadsheet = workbook.active
+    Args:
+        path (str): The path to the input spreadsheet.
 
-    user_map = {}
+    Returns:
+        Tuple[Dict[str, Department], Dict[str, User]]: A tuple containing department and user data dictionaries.
+    """
+    excel_workbook = openpyxl.load_workbook(path)
+    worksheet = excel_workbook.active
+
     department_map = {}
+    user_map = {}
 
-    for row in spreadsheet.iter_rows(2):
-
-        device_name = row[0].value
-        device_id = row[1].value
-        device_group = row[2].value
-        device_os = row[3].value
-        device_enrollment_type = row[4].value
-        device_type = row[5].value
-        device_last_checkin_date = row[6].value
-
-        user_id = row[7].value
-        user_name = row[8].value
-        user_mail = row[9].value
-        user_manager_name = row[10].value
-        user_manager_mail = row[11].value
-        user_job_title = row[12].value
-        user_location = row[13].value
-        user_cost_center = row[14].value
-        department_name = row[15].value
-        device_affected = row[16].value
-        user_affected = row[17].value
-
-        device = Device(id=device_id, affected=device_affected, name=device_name, group=device_group,
-                        os=device_os, enrollment_type=device_enrollment_type, type=device_type,
-                        last_checkin_date=device_last_checkin_date)
-
-        if user_name in user_map:
-            user_map[user_name].add_device(device)
-
+    for device, user, department_name in process_spreadsheet(worksheet):
+        if user.name in user_map:
+            user_map[user.name].add_device(device)
         else:
-            user = User(id=user_id, affected=user_affected, name=user_name, mail=user_mail, manager_name=user_manager_name, manager_mail=user_manager_mail,
-                        job_title=user_job_title, location=user_location, cost_center=user_cost_center, device_list=[device])
             user_map[user.name] = user
 
             if department_name in department_map:
                 department_map[department_name].add_user(user)
-
             else:
-                department = Department(
-                    name=department_name, user_list=[user])
+                department = Department(name=department_name, user_list=[user])
                 department_map[department_name] = department
 
     return department_map, user_map
 
 
 def save_data_to_xlsx(result_map, file_path):
+    """Save data to the output spreadsheet.
+
+    Args:
+        result_map (Dict[str, Department]): A dictionary containing department data.
+        file_path (str): The path to save the output spreadsheet.
+    """
     result_book = openpyxl.Workbook()
     result_sheet = result_book.active
 
@@ -113,15 +169,13 @@ def save_data_to_xlsx(result_map, file_path):
     row_counter = 2
 
     for department in result_map.values():
-
         department_name = department.name
 
-        # Iterate over the set of user-device tuples
         for user in department.user_list:
             for device in user.device_list:
-
-                create_table_row(result_sheet, row_counter, device, user,
-                                 department_name)
+                row = Row(device=device, user=user,
+                          department_name=department_name)
+                create_table_row(result_sheet, row_counter, row)
                 row_counter += 1
 
     result_book.save(file_path)
--- selector/main.py
+++ selector/main.py
@@ -33,24 +33,24 @@ def device_selector_flow(configuration: Config):
     # Open data grabbed from API
     logging.info(f"Reading users data")
     users = open_json(configuration.file_paths['path_user'])
-    logging.info(f"Reading users data was completed")
+    logging.info(f"Reading users data has completed")
     logging.info(f"Reading affected users and devices data")
     affected = open_json(configuration.file_paths['path_affected'])
-    logging.info(f"Reading affected users and devices data was completed")
+    logging.info(f"Reading affected users and devices data has completed")
 
     # Creating departments from users and devices data
     logging.info(f"Converting users data into objects")
     departments = get_data_from_json(users, affected)
-    logging.info(f"Converting users data into objects was completed")
+    logging.info(f"Converting users data into objects has completed")
 
     # Starting device selection process
     logging.info(f"Starting device selection process")
     result = random_selection(
         departments=departments, selection_conditions=configuration.selection_conditions,)
-    logging.info(f"Device selection process was completed")
+    logging.info(f"Device selection process has completed")
 
     # Saving the result in the xlsx table
     logging.info(f"Saving processed data into XLSX table")
     save_data_to_xlsx(result, configuration.file_paths['result_file'])
-    logging.info(f"Saving processed data was completed")
+    logging.info(f"Saving processed data has completed")
     return 0
